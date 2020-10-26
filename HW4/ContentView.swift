4e4db0d9f9ce433e45dd44a276d94c08896f2f90
xxxPatty <L224749391@gmail.com>
2020-10-26 12:31:11 +0800

    Initial Commit

---
+++ HW4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW4
+//
+//  Created by 林湘羚 on 2020/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW4/HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW4App.swift
+//  HW4
+//
+//  Created by 林湘羚 on 2020/10/26.
+//
+
+import SwiftUI
+
+@main
+struct HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
